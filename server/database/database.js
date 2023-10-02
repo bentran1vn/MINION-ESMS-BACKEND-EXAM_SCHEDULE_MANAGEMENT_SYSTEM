821f2a35bef61fa51d1aca3b05a617d2cbaba7cb
EmTanIT <tan182205@gmail.com>
2023-10-02 13:03:25 +0700

    testing new database hosting

--- server/database/database.js
+++ server/database/database.js
@@ -3,6 +3,18 @@ import Sequelize from 'sequelize'
 const sequelize = new Sequelize(process.env.MYSQL_DB, process.env.MYSQL_USERNAME, process.env.MYSQL_PASSWORD, {
     host: process.env.MYSQL_HOST,
     dialect: 'mysql',
+    post: process.env.MYSQL_PORT,
+    logging: false,
+    pool: {
+      max: 100,
+      min: 0,
+      acquire: 1000000,
+      idle: 100000,
+      evict: 2000,
+    },
+    dialectOptions: {
+      decimalNumbers: true,
+    },
 })
 
 sequelize.authenticate().then(() => {
