66c38bb1dbab5d7d5e6cafb818da383ffc1022a2
EmTanIT <tan182205@gmail.com>
2023-10-19 17:43:28 +0700

    Configure Get Semester

--- services/semesterServices.js
+++ services/semesterServices.js
@@ -1,18 +1,5 @@
 import Semester from "../models/Semester.js";
 
-export async function findAllSemesterByStatus(sesStatus) {
-    const semesterList = await Semester.findAll({
-        where: {
-            status : sesStatus
-        }
-    });
-    if(semesterList == null || semesterList.length == 0) {
-        throw new Error("Can not find the List Of Semester!")
-    } else {
-        return semesterList
-    }
-}
-
 export async function createNewSemesterS(season, year, startDay, endDay){
     const semester = await Semester.create({
         season: season,
@@ -40,5 +27,25 @@ export async function deleteSemesterById(semId){
         throw new Error("Can not delete Semester!")
     } else {
         return result
+    }
+}
+
+export async function findAllSemester(value, filterBy) {
+    let whereClause
+    if(filterBy === "year"){
+        whereClause = { year: parseInt(value) }
+    } else if (filterBy === "season"){
+        whereClause = { season: value }
+    } else if (filterBy === "status"){
+        whereClause = { status: parseInt(value) }
+    } else {
+        throw new Error("Can not find the List Of Semester!")
+    }
+    const semesterList = await Semester.findAll({ where: whereClause });
+
+    if (semesterList == null || semesterList.length === 0) {
+        throw new Error("Can not find the List Of Semester!");
+    } else {
+        return semesterList;
     }
 }
